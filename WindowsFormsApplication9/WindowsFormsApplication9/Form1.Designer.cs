6fec19911b2a84bf71da0e7d582de31a2839ec2a
bakhtawar12 <38508929+bakhtawar12@users.noreply.github.com>
2018-04-26 00:23:21 +0500

    Registration form by Ahmad

--- WindowsFormsApplication9/WindowsFormsApplication9/Form1.Designer.cs
+++ WindowsFormsApplication9/WindowsFormsApplication9/Form1.Designer.cs
@@ -123,7 +123,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(222, 247);
+            this.button1.Location = new System.Drawing.Point(214, 250);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(129, 30);
             this.button1.TabIndex = 9;
@@ -146,7 +146,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(488, 326);
+            this.ClientSize = new System.Drawing.Size(541, 375);
             this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label5);
