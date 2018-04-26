b96ad569f48212a2a6439f9864ce76db4905dd78
bakhtawar12 <38508929+bakhtawar12@users.noreply.github.com>
2018-04-26 10:11:50 +0500

    By Ahmad

---
+++ Ahmad/Ahmad/Form3.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ahmad
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Form2 frm = new Form2();
+            frm.Show();
+            this.Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            localhost.Service1 v = new localhost.Service1();
+            v.ischeck(textBox1.Text, textBox2.Text);
+            MessageBox.Show(" Successfully login");
+            textBox1.Text = " ";
+            textBox2.Text = " ";
+
+        }
+    }
+}
